0db3e8af61bd0fea580d7d0b03023d92636874d7
René Avendaño <samuel.benicio.delmar@gmail.com>
2023-08-18 18:38:22 -0400

    Aplicacion terminada hasta la seccion 14 del curso de react de FH

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,12 @@
+import { AppRouter } from "./router/AppRouter"
+import "animate.css";
+
+export const HeroesApp = () => {
+    return (
+        <>
+
+            <AppRouter />
+
+        </>
+    )
+}
---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,19 @@
+import { useMemo } from "react";
+import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./HeroCard";
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+
+    return (
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+            {
+                heroes.map(heroe => (
+                    <HeroCard key={heroe.id} {...heroe} />
+                ))
+            }
+
+        </div>
+    )
+}
